f56998c10bdad3eb2de497746f7b8a2f305b9bb6
Lucas <lukkasbarros@gmail.com>
2022-06-23 18:08:59 -0300

    Adicionando repositório

---
+++ DownloaderGUI-v1.py
@@ -0,0 +1,27 @@
+from tkinter import *
+from pytube import YouTube
+
+root = Tk()
+root.geometry('500x300')
+root.resizable(0,0)
+root.title("Youtube Video Downloader v1.0")
+
+Label(root,text = 'Youtube Video Downloader', font ='arial 20 bold').pack()
+link = StringVar()
+path = StringVar()
+Label(root, text = 'Cole o link abaixo:', font = 'arial 15 bold').place(x= 160 , y = 60)
+link_enter = Entry(root, width = 70,textvariable = link).place(x = 32, y = 90)
+Label(root, text = 'Escreva o caminho para salvar (ex.: /Users/usuário/Videos):', font = 'arial 12 bold').place(x= 25 , y = 125)
+path_enter = Entry(root, width = 70,textvariable = path).place(x = 32, y = 150)
+
+def Downloader():     
+    url = YouTube(str(link.get()))
+    video = url.streams.get_highest_resolution()
+    video.download(path.get())
+    Label(root, text = 'Download efetuado com sucesso', font = 'arial 15').place(x= 110 , y = 250) 
+
+Button(root,text = 'DOWNLOAD', font = 'arial 11 bold' ,bg = 'light cyan', padx = 2, command = Downloader).place(x=180 ,y = 190)
+root.mainloop()
+
+
+
---
+++ README.md
@@ -0,0 +1,6 @@
+# Youtube video downloader em Python com GUI Tkinter
+
+## Como usar?
+Digite o caminho onde deseja salvar o arquivo, cole o link do vídeo no campo de texto e clique no botão "DOWNLOAD".
+
+O título será automaticamente definido para o mesmo do youtube, o arquivo será salvo na pasta escolhida e o tamanho do vídeo será definido automaticamente (configurado para a maior qualidade de vídeo disponível em mp4).
